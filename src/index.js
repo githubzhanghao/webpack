7b566b566dfff4ed9722b658a058279cf4e5aab3
Ryze <459791911@qq.com>
2019-10-14 22:02:27 +0800

    commit

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import _ from 'lodash';
 import '../style/index.css';
-import '../style/index.scss';
+// import '../style/index.scss';
+import imgUrl from '../res/test.jpg';
 
 function createDomElement() {
     let dom = document.createElement('div');
     dom.innerHTML = _.join(['1', '2', 3, 4, 5], '');
     dom.className = 'box';
+    let img = new Image();
+    img.src = imgUrl;
+    dom.appendChild(img);
     return dom;
 }
 
-for (let i = 0; i < 10; i++) {
+// for (let i = 0; i < 10; i++) {
     document.body.appendChild(createDomElement());
-}
+// }
--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,6 @@
 var path = require('path');
-var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// var BundleAnalyzerPlugin = require('webpack-bundle-analyzer').BundleAnalyzerPlugin;
+// var ExtractTextPlugin = require('extract-text-webpack-plugin');
 
 module.exports = {
     mode: 'development',
@@ -9,12 +10,41 @@ module.exports = {
         path: path.resolve(__dirname, 'dist')
     },
     module: {
-        rules: [{
-            test: /\.(sc|sa|c)ss$/,
-            use: ['style-loader', 'css-loader', 'sass-loader']
-        }]
+        rules: [
+            {
+                test: /\.(sc|sa|c)ss$/,
+                // use: ExtractTextPlugin.extract({
+                //     fallback: 'style-loader',
+                //     use: 'css-loader'
+                // })
+                use: ['style-loader', 'css-loader', 'sass-loader']
+            }, 
+            {
+                test: /\.(png|jpg|gif|svg)$/,
+                use: [
+                    'file-loader',
+                    // {
+                    //     loader: 'url-loader',
+                    //     options: {
+                    //         limit: 222000,
+                    //         // name: '[path][name].[ext]?[hash:6]!./dir/file.png'
+                    //     }
+                    // },
+                    {
+                        loader: 'image-webpack-loader',
+                        options: {
+                            mozjpeg: {
+                                progressive: false,
+                                quality: 95
+                            }
+                        }
+                    }
+                ]
+            }
+        ]
     },
     plugins: [
-        new BundleAnalyzerPlugin()
+        // new BundleAnalyzerPlugin()
+        // new ExtractTextPlugin('styles.css')
     ]
 }
